3300b40190bc43c703b3bab96d474f34a84ff99a
ValeriiaKozodaeva <118925270+ValeriiaKozodaeva@users.noreply.github.com>
2022-11-25 11:37:39 +0300

    Add files via upload
    Повторная загрузка с исправленными замечаниями :)

---
+++ Лабораторная 3, новая/Task_9_main.py
@@ -0,0 +1,12 @@
+salary = 5000  # зарплата
+spend = 6000  # траты
+months = 10  # количество месяцев
+increase = 0.03  # рост цен
+money_capital = 0  # количество денег, чтобы прожить 10 месяцев
+
+for i in range(1, months + 1):
+    delta = spend - salary
+    money_capital += delta
+    spend *= 1 + increase
+
+print(round(money_capital))
